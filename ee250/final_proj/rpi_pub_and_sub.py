6ca00e31c0a08f68ee6c5dabe0bc7812a193ff17
amrgn <xmorgan@usc.edu>
2020-11-15 00:33:26 -0800

    test

--- ee250/final_proj/rpi_pub_and_sub.py
+++ ee250/final_proj/rpi_pub_and_sub.py
@@ -11,12 +11,39 @@ sys.path.append('../../Software/Python/grove_rgb_lcd')
 import grovepi
 import grove_rgb_lcd
 
+import json
+
 import paho.mqtt.client as mqtt
 import time
 
 HOSTNAME = "xm_pi/"
 buzzer = 2
 led    = 5
+sound  = 0
+
+NUM_SECONDS = 3
+SAMPLES_PER_SECOND = 20
+NUM_SAMPLES = NUM_SECONDS*SAMPLES_PER_SECOND
+
+sound_data = {
+    "time" : range(NUM_SAMPLES),       # each block represents 0.1 seconds giving 3 seconds of recording total
+    "amplitude" : range(NUM_SAMPLES)  
+}
+for i in range(NUM_SAMPLES)
+    time[i] = i/SAMPLES_PER_SECOND
+
+def get_sound(client, userdata, message):
+    msg = str(message.payload,"utf-8")
+    if msg == "get_sound":
+        for i in range(NUM_SAMPLES):
+            sound_data["amplitude"][i] = grovepi.analogRead(sound)
+            time.sleep(1/SAMPLES_PER_SECOND)
+        send_data = json.dumps(sound_data)
+        client.publish(HOSTNAME + "sound_data", send_data)
+
+    else:
+        print("Invalid get sound request")            
+
 
 def hello_msg(client, userdata, message):
     msg = str(message.payload,"utf-8")
@@ -53,6 +80,7 @@ def hello_msg(client, userdata, message):
 def motion_msg(client, userdata, message):
     msg = str(message.payload,"utf-8")
     if msg== "Motion detected!":
+        print("Phone detected motion!")
         grovepi.digitalWrite(buzzer,1)
         time.sleep(0.5)
         grovepi.digitalWrite(buzzer,0)
@@ -63,7 +91,6 @@ def motion_msg(client, userdata, message):
 
 def led_msg(client, userdata, message):
     msg = float(str(message.payload,"utf-8"))
-    print(msg)
     if msg < 30:
         grovepi.digitalWrite(led, 1)
     else:
@@ -79,6 +106,8 @@ def on_connect(client, userdata, flags, rc):
     client.message_callback_add("xm_phone/motion", motion_msg)
     client.subscribe("xm_phone/light", qos = 2)
     client.message_callback_add("xm_phone/light", led_msg)
+    client.subscribe("xm_phone/get_sound", qos = 2)
+    client.message_callback_add("xm_phone/get_sound", get_sound)
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
@@ -93,6 +122,8 @@ if __name__ == '__main__':
 
     grovepi.pinMode(buzzer, "OUTPUT")
     grovepi.pinMode(led, "OUTPUT")
+    grovepi.pinMode(sound,"INPUT")
+
 
     while True:
         try:
--- ee250/final_proj/vm_subscriber.py
+++ ee250/final_proj/vm_subscriber.py
@@ -5,25 +5,41 @@ Run vm_subscriber.py in a separate terminal on your VM."""
 import paho.mqtt.client as mqtt
 import time
 
+import matplotlib.pyplot as plt
+
+import json
+
+NUM_SECONDS = 3
+SAMPLES_PER_SECOND = 20
+NUM_SAMPLES = NUM_SECONDS*SAMPLES_PER_SECOND
+
+def disp_sound_data(client, userdata, message):
+    msg = str(message.payload,"utf-8")
+    data = json.loads(msg)
+    plt.figure(figsize=(20,10))
+    plt.plot(data["time"],data["amplitude"])
+    plt.xlabel("time")
+    plt.ylabel("amplitude")
+    plt.title("Sound data")
+    plt.show()
+
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    #subscribe to the ultrasonic ranger topic here
+    client.subscribe("xm_pi/sound_data", qos = 2)
+    client.message_callback_add("xm_pi/sound_data", disp_sound_data)
 
-#Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 if __name__ == '__main__':
-    #this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
     client.on_message = on_message
     client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.connect(host="broker.emqx.io", port=1883, keepalive=60)
     client.loop_start()
 
     while True:
-        print("delete this line")
         time.sleep(1)
             
 
